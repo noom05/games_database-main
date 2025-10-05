c8a507981719ae160b89081785e4690b2099d634
noom05 <znoom204@gmail.com>
2025-10-05 23:05:30 +0700

    add path

--- app.ts
+++ app.ts
@@ -4,9 +4,12 @@ import { router as index } from "./controller/index";
 import { router as user} from "./controller/user";
 import { router as admin} from "./controller/admin";
 import bodyParser from "body-parser";
+import path from "path";
 
 export const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+    
 app.use(
     cors({
         origin: "*",
