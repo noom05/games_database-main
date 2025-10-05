cf0451dedb29dddba655526cd70c36658367c946
noom05 <znoom204@gmail.com>
2025-10-05 21:37:25 +0700

    test

--- dist/app.js
+++ dist/app.js
@@ -4,18 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
-
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const index_1 = require("./controller/index");
 const user_1 = require("./controller/user");
 const admin_1 = require("./controller/admin");
 const body_parser_1 = __importDefault(require("body-parser"));
-const path = require("path");
-
 exports.app = (0, express_1.default)();
 
-// ✅ เสิร์ฟไฟล์ static จาก public
+const path = require("path");
 exports.app.use(express_1.default.static(path.join(__dirname, "public")));
 
 exports.app.use((0, cors_1.default)({
