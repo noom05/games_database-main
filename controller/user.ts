16e79888652fcf82e5552b51f1e752c8671d221a
noom05 <znoom204@gmail.com>
2025-10-12 18:06:57 +0700

    18.06

--- controller/user.ts
+++ controller/user.ts
@@ -19,7 +19,7 @@ router.get("/", async (req, res) => {
     console.error(err);
     res.status(500).json({ error: "Cannot read users data" });
   }
-});
+}); 
 
 // --- Wallet & Transaction Routes ---
 
